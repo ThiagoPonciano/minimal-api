461e599a959d2dc1d8dd17a0aad26daca9691731
Thiago Ponciano <thiagovponciano@gmail.com>
2024-10-20 17:13:28 -0300

    Erro ao adicionar AdministradorMigration, buscando solucao

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("mysql");
+
+    options.UseMySql(connectionString, 
+        ServerVersion.AutoDetect(connectionString)); // Usa auto-detect para a versão do MySQL
+});
+
+
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login com sucesso");
