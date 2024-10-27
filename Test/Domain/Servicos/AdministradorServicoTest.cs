00faaa57a6850632f5d621c9e531cc9f7d5a13bf
Thiago Ponciano <thiagovponciano@gmail.com>
2024-10-27 19:03:38 -0300

    Implementando teste e refatorando o projeto

---
+++ Test/Domain/Servicos/AdministradorServicoTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Infraestrutura.Db;
+
+namespace Test.Domain.Servicos
+{
+    [TestClass]
+    public class AdministradorServicoTest
+    {
+        [TestMethod]
+        public void TestandoSalvarAdministrador()
+        {
+            //Arrange
+            var adm = new Administrador();
+            adm.Id = 1;
+            adm.Email = "teste@teste.com";
+            adm.Senha = "teste";
+            adm.Perfil = "Adm";
+
+            // Act
+            
+
+            // Assert
+            Assert.AreEqual(1, adm.Id);
+            Assert.AreEqual("teste@teste.com", adm.Email);
+            Assert.AreEqual("teste", adm.Senha);
+            Assert.AreEqual("Adm", adm.Perfil);
+        }
+    }
+}
